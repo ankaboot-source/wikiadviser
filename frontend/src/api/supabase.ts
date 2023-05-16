8d0a74e177681550a9f9f030f82b6f0a8063b8ca
Jaafoura <mehdi.jaafourag@gmail.com>
2023-05-16 15:04:28 +0100

    clean

--- frontend/src/api/supabase.ts
+++ frontend/src/api/supabase.ts
@@ -1,10 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_PROJECT_URL = process.env.SUPABASE_PROJECT_URL;
-const SUPABASE_SECRET_PROJECT_TOKEN = process.env.SUPABASE_SECRET_PROJECT_TOKEN;
-
 const supabaseClient = createClient(
-  SUPABASE_PROJECT_URL!,
-  SUPABASE_SECRET_PROJECT_TOKEN!
+  process.env.SUPABASE_PROJECT_URL!,
+  process.env.SUPABASE_SECRET_PROJECT_TOKEN!
 );
 export default supabaseClient;

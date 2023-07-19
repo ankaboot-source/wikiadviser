5ef7ea7b93c52abe4f63fe7c68decd327bec5398
Jaafoura <mehdi.jaafourag@gmail.com>
2023-07-19 13:32:24 +0100

    frontend: selectable roles + UI

--- frontend/src/components/ShareCard.vue
+++ frontend/src/components/ShareCard.vue
@@ -1,22 +1,42 @@
 <template>
-  <q-card style="min-width: 25vw" class="q-px-lg q-mb-xl bg-secondary">
-    <q-card-section>
-      <p class="text-h5 text-center merriweather">Share</p>
-    </q-card-section>
-    <q-card-section class="q-pb-xl">
-      <q-list bordered separator>
-        <q-item v-for="user in users" :key="user.username">
-          <share-user :user="user" :role="role"></share-user>
-        </q-item>
-      </q-list>
-      <q-btn
-        icon="link"
-        color="primary"
-        outline
-        class="q-mt-md"
-        label="share article"
-        @click="copyValueToClipboard()"
-      ></q-btn>
+  <q-card
+    style="min-width: 30vw"
+    class="q-px-lg q-mb-xl bg-secondary full-height row"
+  >
+    <q-card-section class="q-pb-xl column col">
+      <q-card-section>
+        <p class="text-h5 text-center merriweather">Share</p>
+      </q-card-section>
+      <q-scroll-area class="col-grow">
+        <q-list bordered separator>
+          <share-user
+            v-for="user in users"
+            :key="user.username"
+            :user="user"
+            :role="role"
+          ></share-user>
+        </q-list>
+      </q-scroll-area>
+
+      <q-card-actions class="q-mt-md">
+        <q-btn
+          icon="link"
+          color="primary"
+          outline
+          no-caps
+          label="Copy link"
+          @click="copyValueToClipboard()"
+        />
+        <q-space />
+        <q-btn v-close-popup color="primary" outline no-caps label="Cancel" />
+        <q-btn
+          color="primary"
+          unelevated
+          no-caps
+          label="Apply"
+          @click="handleRoleChange()"
+        />
+      </q-card-actions>
     </q-card-section>
   </q-card>
 </template>
@@ -25,7 +45,7 @@
 import { onMounted, ref } from 'vue';
 import { User, UserRole } from 'src/types';
 import ShareUser from './ShareUser.vue';
-import { getUsers } from 'src/api/supabaseHelper';
+import { getUsers, updatePermission } from 'src/api/supabaseHelper';
 import { useRoute } from 'vue-router';
 const route = useRoute();
 import { copyToClipboard, useQuasar } from 'quasar';
@@ -47,5 +67,14 @@ async function copyValueToClipboard() {
     color: 'positive',
     icon: 'content_copy',
   });
+}
+
+async function handleRoleChange() {
+  return;
+  //Set permission: parse through roles
+  await updatePermission(
+    props.user.permissionId,
+    UserRole[roleModel.value as keyof typeof UserRole]
+  );
 }
 </script>
--- frontend/src/components/ShareUser.vue
+++ frontend/src/components/ShareUser.vue
@@ -1,19 +1,41 @@
 <template>
-  <q-item-section>
-    <q-item-label>{{ props.user.username }}</q-item-label>
-    <q-item-label caption>{{ props.user.email }}</q-item-label>
-    <q-select
-      v-model="roleModel"
-      :options="roleOptions"
-      label="Role"
-      :disable="!!role || roleModel == 'Owner'"
-      @update:model-value="handleRoleChange()"
-    />
-  </q-item-section>
+  <q-expansion-item>
+    <template #header>
+      <q-item-section avatar>
+        <q-avatar size="md" icon="person" color="accent" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ props.user.username }}</q-item-label>
+        <q-item-label caption>{{ props.user.email }}</q-item-label>
+      </q-item-section>
+      <q-item-section side>
+        <q-avatar
+          v-if="!roleModel[0]"
+          color="yellow-8"
+          icon="priority_high"
+          size="24px"
+        >
+          <q-tooltip anchor="top middle" self="bottom middle">
+            User needs attention.
+          </q-tooltip>
+        </q-avatar>
+      </q-item-section>
+    </template>
+    <q-item-section>
+      <q-option-group
+        v-model="roleModel"
+        class="q-ml-sm q-my-sm"
+        :options="roleOptions"
+        dense
+        label="Role"
+        :disable="!!role"
+        type="checkbox"
+      />
+    </q-item-section>
+  </q-expansion-item>
 </template>
 
 <script setup lang="ts">
-import { updatePermission } from 'src/api/supabaseHelper';
 import { User, UserRole } from 'src/types';
 import { ref } from 'vue';
 
@@ -26,15 +48,25 @@ const roleDictionary: Record<UserRole, string> = {
   [UserRole.Contributor]: 'Contributor',
   [UserRole.Reviewer]: 'Reviewer',
 };
-const roleModel = ref<string>(roleDictionary[props.user.role as UserRole]);
-const roleOptions = ['Contributor', 'Reviewer'];
-
-async function handleRoleChange() {
-  //Set permission
-  console.log(roleModel.value);
-  await updatePermission(
-    props.user.permissionId,
-    UserRole[roleModel.value as keyof typeof UserRole]
-  );
+const roleModel = ref<string[]>([roleDictionary[props.user.role as UserRole]]);
+console.log(roleModel.value);
+const roleOptions = [
+  {
+    value: 'Contributor',
+    label: 'Contributor',
+    disable: false,
+  },
+  {
+    value: 'Reviewer',
+    label: 'Reviewer',
+    disable: false,
+  },
+];
+if (roleModel.value.includes('Owner')) {
+  roleOptions.unshift({
+    value: 'Owner',
+    label: 'Owner',
+    disable: true,
+  });
 }
 </script>

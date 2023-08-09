72d541fcb6e4d8fbd926eddb2b14cb46ec1ae75e
Chedly Zouche <chedly.zouche@pm.me>
2023-08-09 11:05:59 +0100

    Update cors

--- backend/src/middleware/cors.ts
+++ backend/src/middleware/cors.ts
@@ -1,18 +1,8 @@
 import cors, { CorsOptions } from 'cors';
 
 const corsOptions: CorsOptions = {
-  origin: [
-    process.env.WIKIADVISER_FRONTEND_HOST ?? 'https://app.wikiadviser.io'
-  ],
-  methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-  allowedHeaders: [
-    'Authorization',
-    'X-Requested-With',
-    'Content-type',
-    'x-sb-jwt'
-  ],
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
-  credentials: true
+  origin: process.env.WIKIADVISER_FRONTEND_HOST ?? 'https://app.wikiadviser.io',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204,
 };
 const corsMiddleware = cors(corsOptions);
 

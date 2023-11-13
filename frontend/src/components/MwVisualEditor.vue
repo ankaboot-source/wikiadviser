cdbe3822fab52e6214a9d16f7fa5dcfc89ee3e74
Jaafoura <mehdi.jaafourag@gmail.com>
2023-11-13 10:21:47 +0100

    frontend: fixed iframe link

--- frontend/src/components/MwVisualEditor.vue
+++ frontend/src/components/MwVisualEditor.vue
@@ -9,5 +9,5 @@ const props = defineProps({
   article: { type: Object as () => Article, required: true },
 });
 
-const articleLink = `${process.env.MEDIAWIKI_HOST}/wiki/${props.article.article_id}?veaction=edit&permissionid=${props.article.permission_id}&expectedTitle=${props.article.title}`;
+const articleLink = `${process.env.MEDIAWIKI_HOST}/w/index.php/${props.article.article_id}?veaction=edit&permissionid=${props.article.permission_id}&expectedTitle=${props.article.title}`;
 </script>

f3615640a4ad8a3329f56f493af2df8887c1390c
Malek salem <malek@ankaboot.io>
2023-11-29 12:33:38 +0100

    180 authentication UI fixes (#241)

--- frontend/src/pages/auth/AuthenticationPage.vue
+++ frontend/src/pages/auth/AuthenticationPage.vue
@@ -18,6 +18,9 @@
                   brand: 'hsl(207, 90%, 49%)',
                   brandAccent: 'hsl(207, 85%, 41%)',
                 },
+                fontSizes: {
+                  baseBodySize: '15px',
+                },
               },
             },
           }"
@@ -32,9 +35,40 @@
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { Auth } from '@nuxtbase/auth-ui-vue';
 import supabaseClient from 'src/api/supabase';
+import { onMounted } from 'vue';
 
 const callbackURL = `${location.origin}/auth/callback`;
 const providerScopes = {
   azure: 'email',
 };
+
+onMounted(() => {
+  // Replaces Azure name & logo with Microsoft.
+  const signInButton = Array.from(
+    document.querySelectorAll('.supabase-auth-ui_ui-button')
+  ).find((button) => button.textContent === 'Sign in with Azure');
+
+  if (signInButton) {
+    const newSignInButton = document.createElement('button');
+    newSignInButton.className =
+      'supabase-auth-ui_ui-button c-bOcPnF c-bOcPnF-iwjZXY-color-default';
+    newSignInButton.innerHTML = `
+      <svg 
+        xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 30 30"
+          width="21px"
+          height="21px"
+          style="margin-left: 17px;"
+        >
+          <path fill="#f3f3f3" d="M0 0h23v23H0z"/>
+          <path fill="#f35325" d="M1 1h10v10H1z"/>
+          <path fill="#81bc06" d="M12 1h10v10H12z"/>
+          <path fill="#05a6f0" d="M1 12h10v10H1z"/>
+          <path fill="#ffba08" d="M12 12h10v10H12z"/>
+        </svg>
+        Sign in with Microsoft
+      `;
+    signInButton.parentNode?.replaceChild(newSignInButton, signInButton);
+  }
+});
 </script>

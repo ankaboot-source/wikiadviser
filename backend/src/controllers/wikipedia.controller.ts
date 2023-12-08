fd3c336444f54b2aa58d30bb7fc00794cf1464d7
Malek salem <malek@ankaboot.io>
2023-12-08 10:55:47 +0100

    add global error handler (#270)
    * add globa error handler
    
    * add global error handler

--- backend/src/controllers/wikipedia.controller.ts
+++ backend/src/controllers/wikipedia.controller.ts
@@ -1,5 +1,4 @@
-import { Request, Response } from 'express';
-import logger from '../logger';
+import { NextFunction, Request, Response } from 'express';
 import wikipediaApi from '../services/wikipedia/WikipediaApi';
 
 /**
@@ -8,7 +7,11 @@ import wikipediaApi from '../services/wikipedia/WikipediaApi';
  * @param {Request} req - The Express request object.
  * @param {Response} res - The Express response object.
  */
-export default async function getWikipediaArticle(req: Request, res: Response) {
+export default async function getWikipediaArticle(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   const { term, language } = req.query;
   try {
     if (typeof term !== 'string' || typeof language !== 'string') {
@@ -16,19 +19,11 @@ export default async function getWikipediaArticle(req: Request, res: Response) {
     }
 
     const response = await wikipediaApi.getWikipediaArticles(term, language);
-    res.status(200).json({
+    return res.status(200).json({
       message: 'Getting Wikipedia articles succeeded.',
       searchResults: response
     });
   } catch (error) {
-    let message = 'Getting Wikipedia articles failed.';
-
-    if (error instanceof Error) {
-      message = error.message;
-      logger.error(error.message);
-    } else {
-      logger.error(error);
-    }
-    res.status(500).json({ message });
+    return next(error);
   }
 }

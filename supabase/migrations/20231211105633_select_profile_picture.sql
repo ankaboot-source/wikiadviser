93a76e381b2e59ae14d5a518e4b807acf6bc5edb
Malek salem <malek@ankaboot.io>
2023-12-11 14:10:38 +0100

    add user profile picture (#282)
    * use profile picture
    
    * add new sql trigger

---
+++ supabase/migrations/20231211105633_select_profile_picture.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION handle_user_profile_picture()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF NEW.raw_user_meta_data->>'picture' IS NULL THEN
+    NEW.raw_user_meta_data = jsonb_set(
+        NEW.raw_user_meta_data::jsonb,
+        '{picture}',
+        to_jsonb('https://www.gravatar.com/avatar/' || md5(NEW.email) || '?d=https://ui-avatars.com/api/' || NEW.email || '/128/random/fff/1'),
+        true
+    );
+    END IF;
+  
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER handle_profile_picture_trigger
+BEFORE INSERT
+ON auth.users
+FOR EACH ROW
+EXECUTE FUNCTION handle_user_profile_picture();

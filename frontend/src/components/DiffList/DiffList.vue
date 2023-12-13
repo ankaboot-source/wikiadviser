54e6883c7a841f4178bfec833793f1f3334cfd54
Jaafoura <mehdi.jaafourag@gmail.com>
2023-12-13 16:46:13 +0100

    frontend: revisions

--- frontend/src/components/DiffList/DiffList.vue
+++ frontend/src/components/DiffList/DiffList.vue
@@ -2,14 +2,57 @@
   <div class="column">
     <div class="text-h6 q-pb-sm">Validate Changes</div>
     <q-scroll-area v-if="props.changesList.length" class="col-grow">
-      <q-list>
-        <diff-item
-          v-for="item in indexedChanges"
-          :key="item.id"
-          :item="item"
-          :role="role"
-        />
-      </q-list>
+      <q-expansion-item
+        v-for="revision in groupedIndexedChanges"
+        :key="revision.revision"
+        v-model="expanded"
+        style="border-radius: 4px"
+        class="q-mb-md q-mx-sm borders bg-accent"
+      >
+        <template #header>
+          <q-item-section class="text-body1">
+            <q-item-label>
+              Revision {{ revision.revision }}
+              <br />
+              <q-badge
+                text-color="light-blue-1"
+                color="light-blue-10"
+                class="q-mt-s text-capitalize"
+                :label="`${revision.items.length} changes`"
+                size="sm"
+              />
+            </q-item-label>
+
+            <q-item-label v-if="!expanded" caption lines="2">
+              <q-tooltip>{{ description }}</q-tooltip>
+              <div>{{ description }}</div>
+            </q-item-label>
+          </q-item-section>
+          <q-item-section caption top side lines="2">
+            <span class="text-black">
+              <q-avatar size="sm" icon="person" color="accent" />
+              {{ revision.items[0]?.user.email }}</span
+            >
+            <span style="size: 0.5rem">
+              {{ new Date(revision.items[0]?.created_at).toLocaleTimeString() }}
+              <br />
+              {{ new Date(revision.items[0]?.created_at).toLocaleDateString() }}
+            </span>
+          </q-item-section>
+        </template>
+
+        <q-list>
+          <q-item-label class="q-ma-sm">
+            <div>{{ description }}</div>
+          </q-item-label>
+          <diff-item
+            v-for="item in revision.items"
+            :key="item.id"
+            :item="item"
+            :role="role"
+          />
+        </q-list>
+      </q-expansion-item>
 
       <q-expansion-item
         v-if="unindexedChanges.length"
@@ -40,7 +83,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import DiffItem from './DiffItem.vue';
 import { ChangesItem, UserRole } from 'src/types';
 
@@ -52,6 +95,23 @@ const props = defineProps<{
 const indexedChanges = computed(() =>
   props.changesList.filter((item) => item.index !== null)
 );
+const expanded = ref(false);
+
+const description = 'Revision description';
+const groupedIndexedChanges = computed(() => {
+  const grouped = new Map<number, { revision: number; items: ChangesItem[] }>();
+
+  indexedChanges.value.forEach((item) => {
+    const revision = item.revision;
+    if (!grouped.has(revision)) {
+      grouped.set(revision, { revision, items: [] });
+    }
+    grouped.get(revision)?.items.push(item);
+  });
+  return Array.from(grouped.values()).sort((a, b) => b.revision - a.revision);
+});
+
+console.log(groupedIndexedChanges.value);
 const unindexedChanges = computed(() =>
   props.changesList.filter((item) => item.index === null)
 );

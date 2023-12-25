bd11f57405d894ba09f22f05358e1d6f4a56957c
Jaafoura <mehdi.jaafourag@gmail.com>
2023-12-25 12:07:21 +0100

    Revision n°X the Date

--- frontend/src/components/DiffList/DiffList.vue
+++ frontend/src/components/DiffList/DiffList.vue
@@ -73,10 +73,7 @@ const props = defineProps<{
   changesList: ChangesItem[];
 }>();
 
-const indexedChanges = computed(() =>
-  props.changesList.filter((item) => item.index !== null && !item.archived)
-);
-const groupedIndexedChanges = computed(() => {
+const groupedChanges = computed(() => {
   const grouped = new Map<
     number,
     {
@@ -86,7 +83,7 @@ const groupedIndexedChanges = computed(() => {
     }
   >();
 
-  indexedChanges.value.forEach((item) => {
+  props.changesList.forEach((item) => {
     const revision = item.revision.revid;
     const summary = item.revision.summary;
     if (!grouped.has(revision)) {
@@ -100,8 +97,26 @@ const groupedIndexedChanges = computed(() => {
     grouped.get(revision)?.items.push(item);
   });
 
-  return Array.from(grouped.values()).sort((a, b) => b.revision - a.revision);
+  const sortedGrouped = Array.from(grouped.values()).sort(
+    (a, b) => b.revision - a.revision
+  );
+
+  return sortedGrouped.map((item, index) => ({
+    index: sortedGrouped.length - 1 - index,
+    ...item,
+  }));
 });
+
+const groupedIndexedChanges = computed(() =>
+  groupedChanges.value
+    .map((groupedItem) => ({
+      ...groupedItem,
+      items: groupedItem.items.filter(
+        (item) => item.index !== null && !item.archived
+      ),
+    }))
+    .filter((groupedItem) => groupedItem.items.length > 0)
+);
 
 const archivedChanges = computed(() =>
   props.changesList.filter((item) => item.archived)
@@ -109,7 +124,6 @@ const archivedChanges = computed(() =>
 const unindexedChanges = computed(() =>
   props.changesList.filter((item) => item.index === null)
 );
-
 const pastChanges = computed(() =>
   archivedChanges.value.concat(unindexedChanges.value)
 );
--- frontend/src/components/DiffList/RevisionItem.vue
+++ frontend/src/components/DiffList/RevisionItem.vue
@@ -4,7 +4,7 @@
       <q-item-section class="text-body">
         <q-item-label>
           {{
-            `Revision of ${new Date(
+            `Revision n°${revision.index} the ${new Date(
               revision.items[0]?.created_at
             ).toLocaleDateString()} at ${new Date(
               revision.items[0]?.created_at
@@ -57,6 +57,7 @@ import { useSelectedChangeStore } from 'src/stores/useSelectedChangeStore';
 const props = defineProps<{
   role: UserRole;
   revision: {
+    index: number;
     revision: number;
     summary: string;
     items: ChangesItem[];

b5b8d76a12d2520f5e83e738885c45250aca0568
Jaafoura <mehdi.jaafourag@gmail.com>
2024-01-03 12:18:49 +0100

    add NODE_ENV

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import logger from '../logger';
 import { ErrorResponse } from '../types';
 
+const { NODE_ENV } = process.env;
+
 export default function errorHandler(
   error: Error,
   _req: Request,
@@ -13,6 +15,12 @@ export default function errorHandler(
   const response: ErrorResponse = {
     message: error.message
   };
+
+  if (NODE_ENV !== 'production') {
+    if (error.stack) {
+      response.stack = error.stack;
+    }
+  }
 
   logger.error(error);
   return res.status(code).send({ ...response });

cfb953324723688cf7f5e6147e3ec64274a7f667
Jaafoura <73950268+J43fura@users.noreply.github.com>
2024-01-04 15:33:58 +0100

    356 move changes to right alignment fixes (#361)
    * Update ArticlePage.vue
    
    * changes to right alignement fixes
    
    * Update MwVisualEditor.vue
    
    * Update DiffCard.vue

--- frontend/src/components/MwVisualEditor.vue
+++ frontend/src/components/MwVisualEditor.vue
@@ -3,13 +3,13 @@
     v-if="renderIframe"
     v-show="buttonToggle === 'edit' && !loading.value"
     :src="articleLink"
-    class="col-grow q-mr-md rounded-borders borders bg-secondary"
+    class="col-grow rounded-borders borders bg-secondary"
     frameBorder="0"
     @load="loading.value = false"
   />
   <div
     v-if="buttonToggle === 'edit' && loading.value"
-    class="q-pa-xl row justify-center text-center col-grow q-mr-md rounded-borders borders bg-secondary"
+    class="q-pa-xl row justify-center text-center col-grow rounded-borders borders bg-secondary"
   >
     <div>
       <div class="text-h6">

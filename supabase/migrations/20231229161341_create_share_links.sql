36a2d1bdaaf402986fcacf9e4478caf750aff8ca
MBATheGamer-Job <152849306+MBATheGamer-Job@users.noreply.github.com>
2024-01-10 12:26:40 +0100

    Public/Private Share Link (#385)
    * Fix redirecting to article
    
    * Rename path to articleId

--- supabase/migrations/20231229161341_create_share_links.sql
+++ supabase/migrations/20231229161341_create_share_links.sql
@@ -6,42 +6,37 @@ create table "public"."share_links" (
 );
 
 CREATE UNIQUE INDEX share_links_pkey ON public.share_links USING btree (id);
-alter table "public"."share_links" add constraint "share_links_pkey" PRIMARY KEY using index "share_links_pkey";
-alter table "public"."share_links" add constraint "share_links_article_id_fkey" FOREIGN KEY (article_id) REFERENCES articles(id) ON DELETE CASCADE not valid;
+alter table "public"."share_links"
+add constraint "share_links_pkey" PRIMARY KEY using index "share_links_pkey";
+alter table "public"."share_links"
+add constraint "share_links_article_id_fkey" FOREIGN KEY (article_id) REFERENCES articles(id) ON DELETE CASCADE not valid;
 alter table "public"."share_links" validate constraint "share_links_article_id_fkey";
 
 -- add trigger to table
 CREATE FUNCTION delete_old_share_links() RETURNS trigger
-  LANGUAGE plpgsql
-  AS $$
+LANGUAGE plpgsql AS $$
 BEGIN
-  DELETE FROM "public"."share_links" WHERE expired_at < NOW();
+  DELETE FROM "public"."share_links"
+  WHERE expired_at < NOW();
+
   RETURN NEW;
 END;
 $$;
 
 CREATE TRIGGER delete_old_share_links_trigger
-AFTER INSERT ON "public"."share_links"
-EXECUTE PROCEDURE delete_old_share_links();
+AFTER INSERT
+ON "public"."share_links" EXECUTE PROCEDURE delete_old_share_links();
 
 -- add viewer function
-CREATE OR REPLACE FUNCTION add_viewer_to_article(user_id uuid, token uuid)
-RETURNS boolean
-LANGUAGE plpgsql
-AS $$
-DECLARE
-  share_record share_links%ROWTYPE;
+CREATE OR REPLACE FUNCTION add_viewer_to_article(user_id uuid, token uuid) RETURNS uuid
+LANGUAGE plpgsql AS $$
+DECLARE share_record share_links %ROWTYPE;
 BEGIN
-  SELECT *
-  INTO share_record
+  SELECT * INTO share_record
   FROM share_links
   WHERE id = token
   AND expired_at >= NOW()
   LIMIT 1;
-
-  IF NOT FOUND THEN
-    RETURN false;
-  END IF;
 
   INSERT INTO permissions(user_id, article_id, role)
   VALUES (
@@ -50,7 +45,7 @@ BEGIN
     'viewer'
   );
 
-  RETURN true;
+  RETURN share_record.article_id;
 END;
 $$;
 

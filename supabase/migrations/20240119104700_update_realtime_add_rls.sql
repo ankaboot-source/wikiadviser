84196fde79942ea9838aff8b625de885d9c2ec43
Malek salem <malek@ankaboot.io>
2024-01-19 13:13:06 +0100

    update comments

--- supabase/migrations/20240119104700_update_realtime_add_rls.sql
+++ supabase/migrations/20240119104700_update_realtime_add_rls.sql
@@ -2,13 +2,11 @@
 ALTER publication supabase_realtime ADD TABLE public.changes;
 
 -- Add read policy for revisions
--- Policy to allow selects to changes by owners, editors, and reviewers.
 CREATE POLICY select_revisions_policy
   ON changes
   FOR SELECT
   TO authenticated
   USING (
-      -- Check if the authenticated user has appropriate permissions
       auth.uid() IN (
         SELECT user_id
         FROM private.get_all_permissions_by_article_id(article_id) p

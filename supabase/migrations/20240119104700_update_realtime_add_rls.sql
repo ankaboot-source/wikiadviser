0a10094097ada7494ac43794c092d2b75c311572
Malek salem <malek@ankaboot.io>
2024-01-19 12:48:05 +0100

    enable realtime for changes & add rsl policies

---
+++ supabase/migrations/20240119104700_update_realtime_add_rls.sql
@@ -0,0 +1,33 @@
+-- Add realtime for table changes
+ALTER publication supabase_realtime ADD TABLE public.changes;
+
+-- Add read policy for revisions
+-- Policy to allow selects to changes by owners, editors, and reviewers.
+CREATE POLICY select_revisions_policy
+  ON changes
+  FOR SELECT
+  TO authenticated
+  USING (
+      -- Check if the authenticated user has appropriate permissions
+      auth.uid() IN (
+        SELECT user_id
+        FROM private.get_all_permissions_by_article_id(article_id) p
+        WHERE p.role IN ('owner', 'editor', 'reviewer')
+      )
+  );
+
+-- Add read policy for comments
+CREATE POLICY select_comments_policy
+  ON comments
+  FOR INSERT
+  TO authenticated
+  USING (
+    auth.uid() IN (
+          SELECT user_id
+          FROM changes c
+          INNER JOIN permissions p ON 
+              p.article_id = c.article_id
+              AND p.role IN ('owner', 'reviewer', 'editor')
+          WHERE c.id = change_id
+    )
+  );

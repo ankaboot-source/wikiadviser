73e6c57b7d8243214fd41f26671938931aa3b88f
Jaafoura <73950268+J43fura@users.noreply.github.com>
2024-02-06 17:04:39 +0100

    Fix whitespace not showing in DiffCard
    Resolves #445

--- frontend/src/components/DiffCard.vue
+++ frontend/src/components/DiffCard.vue
@@ -278,9 +278,6 @@ b {
   border-bottom: 3px solid rgba(109, 18, 41, 0.5);
   border-top: 3px solid rgba(109, 18, 41, 0.5);
 }
-[data-id] > p {
-  display: inherit;
-}
 
 /* Insert */
 [data-diff-action='insert'],
@@ -414,5 +411,9 @@ figure[data-diff-action='insert'] {
 /* Solves: beginning of article not displaying correctly #400 */
 .ve-ce-leafNode.ve-ce-focusableNode.ve-ce-mwTransclusionNode.ve-ce-focusableNode-invisible {
   display: none;
+}
+
+[data-id]:has(> p) {
+  display: flow-root;
 }
 </style>

b7167ef25e00519a8be765c3762845d3c7fbe668
Jaafoura <mehdi.jaafourag@gmail.com>
2024-02-16 12:38:09 +0100

    Update supabaseCookiesStore.ts

--- frontend/src/utils/supabaseCookiesStore.ts
+++ frontend/src/utils/supabaseCookiesStore.ts
@@ -4,10 +4,14 @@ import { CookieOptions } from '@supabase/ssr';
 const SupabaseCookiesStore = {
   set(key: string, value: string, options: CookieOptions) {
     const cookie = JSON.parse(value);
-    cookie.user = {
-      id: cookie.user.id,
-    };
-    Cookies.set(key, JSON.stringify(cookie), options);
+    if (cookie?.user?.id) {
+      cookie.user = {
+        id: cookie.user.id,
+      };
+      Cookies.set(key, JSON.stringify(cookie), options);
+    } else {
+      Cookies.set(key, value, options);
+    }
   },
 
   get(key: string) {

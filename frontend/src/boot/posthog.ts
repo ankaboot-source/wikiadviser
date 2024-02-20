ef1bda353b8030c052c5c8ba70f0ccf3ff82c84c
Jaafoura <mehdi.jaafourag@gmail.com>
2024-02-20 10:49:31 +0100

    Update posthog.ts

--- frontend/src/boot/posthog.ts
+++ frontend/src/boot/posthog.ts
@@ -2,7 +2,7 @@ import posthog from 'posthog-js';
 import { boot } from 'quasar/wrappers';
 
 export default boot(({ app }) => {
-  posthog.init(process.env.POSTHOG_API_KEY!, {
+  posthog.init(process.env.POSTHOG_API_KEY as string, {
     api_host: process.env.POSTHOG_API_HOST,
   });
   app.provide('posthog', posthog);

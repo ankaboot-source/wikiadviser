55d2abed216e4c1f6fad34dce410746d5e174196
Jaafoura <mehdi.jaafourag@gmail.com>
2024-03-14 11:58:50 +0100

    Use `.env.example` as env for testing

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     setupFiles: ['dotenv/config'],
+    env: {
+      DOTENV_CONFIG_PATH: '.env.example'
+    },
     coverage: {
       reporter: ['cobertura', 'text']
     }

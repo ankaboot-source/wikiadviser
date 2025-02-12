beb429e6e95af44c2f3561fd1a8b116f629c72cc
TejBenamor <tejeddin@ankaboot.io>
2025-02-12 15:21:37 +0100

    Delete unnecessary comment

--- docs/prepare_VisualEditor_upgrade.sh
+++ docs/prepare_VisualEditor_upgrade.sh
@@ -18,12 +18,6 @@ git clone -b "$1" https://gerrit.wikimedia.org/r/mediawiki/extensions/VisualEdit
 # 2. Searchs for files containing "WikiAdviser" within MyVisualEditor and open them in both MyVisualEditor & the cloned VisualEditor folder in VScode
 grep -lir WikiAdviser MyVisualEditor | xargs -I {} sh -c 'code -r "$1" "VisualEditor${1#MyVisualEditor}"' _ {}
 
-# 3. Manually update the code in VisualEditor to match the changes in MyVisualEditor
-#Our custom code is marked by /* Custom WikiAdviser */ comments to help identify what needs to be copied.
-#    Verify changes in the following files:
-#      - ve.init.mw.ProgressBarWidget.js
-#      - ve.ui.DiffElement.js
-#      - ve.init.mw.DesktopArticleTarget.init.js
-#      - ve.init.mw.Target.js
-#      - ve.init.mw.ArticleTarget.js
-#After verification, delete MyVisualEditor and rename VisualEditor to MyVisualEditor
+# 3. Manually update the code in VisualEditor to match the changes in MyVisualEditor.
+# Our custom code is marked by /* Custom WikiAdviser */ comments to help identify what needs to be copied.
+# After verification, delete MyVisualEditor and rename VisualEditor to MyVisualEditor.

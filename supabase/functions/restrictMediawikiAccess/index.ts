43f8125084c298e20fbdf57759016fa0d67f1ade
Jaafoura <mehdi.jaafourag@gmail.com>
2025-04-21 17:38:06 +0100

    use authorizationMiddleware

--- supabase/functions/restrictMediawikiAccess/index.ts
+++ supabase/functions/restrictMediawikiAccess/index.ts
@@ -1,10 +1,10 @@
 import { Hono } from "hono";
-import honoAuthorizationMiddleware from "../_shared/middleware/auth.ts";
+import authorizationMiddleware from "../_shared/middleware/auth.ts";
 import restrictMediawikiAccess from "./controller.ts";
 
 const functionName = "restrictMediawikiAccess";
 const app = new Hono().basePath(`/${functionName}`);
 
-app.get("/", honoAuthorizationMiddleware, restrictMediawikiAccess);
+app.get("/", authorizationMiddleware, restrictMediawikiAccess);
 
 Deno.serve(app.fetch);
--- supabase/functions/share_links/index.ts
+++ supabase/functions/share_links/index.ts
@@ -1,14 +1,14 @@
 import cors from "cors";
 import express from "express";
 
-import { authorizeUser } from "../_shared/middlewares.ts";
+import authorizationMiddleware from "../_shared/middleware/supabaseUserAuthorization.ts";
 import { createShareLink, verifyShareLink } from "./controllers.ts";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(authorizeUser);
+app.use(authorizationMiddleware);
 
 app.post("/share_links", createShareLink);
 app.get("/share_links/:token", verifyShareLink);

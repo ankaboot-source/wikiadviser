d31f7dccb4752bec373c8326b0c4a86028f15226
Jaafoura <73950268+J43fura@users.noreply.github.com>
2025-04-22 18:44:05 +0100

    backend: remove wikipedia route (#821)

--- backend/src/routes/wikipedia.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import getWikipediaArticle from '../controllers/wikipedia.controller';
-
-const wikipediaRouter = Router();
-
-wikipediaRouter.get('/wikipedia/articles', getWikipediaArticle);
-
-export default wikipediaRouter;

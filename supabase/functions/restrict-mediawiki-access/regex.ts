4967bb2b4e4e43c4ce1cc8c8db2429e64a01db32
Jaafoura <mehdi.jaafourag@gmail.com>
2025-04-24 15:35:35 +0100

    This is a test

--- supabase/functions/restrict-mediawiki-access/regex.ts
+++ supabase/functions/restrict-mediawiki-access/regex.ts
@@ -10,3 +10,5 @@ export const allowedPrefixRegEx = new RegExp(
   `^/wiki/(${wikiadviserLanguagesRegex})/(${articleIdRedirectableRegEx})|(images/thumb|load.php\\?|api.php\\?action=(editcheckreferenceurl|query|templatedata)&format=json&(formatversion=2&)?(url|meta=(filerepoinfo|siteinfo)|(revids=\\d+&prop=mapdata|titles=)|prop=(imageinfo(&indexpageids=1&iiprop=size%7Cmediatype|&iiprop=url&iiurlwidth=300&iiurlheight=)?|info%7Cpageprops%7C(pageimages%7Cdescription&pithumbsize=80&pilimit=1&ppprop=disambiguation%7Chiddencat|description&generator=prefixsearch&gpssearch=))(&titles=)?)|(skins|resources|images/timeline)/|extensions/(UniversalLanguageSelector|Kartographer|wikihiero))`,
   "i"
 );
+
+// This is a test

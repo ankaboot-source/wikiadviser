d1ab05548c207fea4c84d0aaa31e3cefa71ce6d6
TejBenamor <tejeddin@ankaboot.io>
2025-04-25 12:36:30 +0100

    refactor: remove unused environment variables from schema to simplify configuration

--- supabase/functions/_shared/schema/env.schema.ts
+++ supabase/functions/_shared/schema/env.schema.ts
@@ -14,24 +14,9 @@ const envSchema = z.object({
       }
       return str.split(",");
     }),
-  WIKIADVISER_API_PORT: z.coerce
-    .number()
-    .positive()
-    .max(65536, "Port should be >= 0 and < 65536")
-    .default(3000),
-  WIKIADVISER_FRONTEND_ENDPOINT: z
-    .string({
-      required_error: "😱 You forgot to add a front-end URL!",
-    })
-    .url(),
   WIKIPEDIA_PROXY: z
     .string({
       required_error: "😱 You forgot to add a Wikipedia proxy URL!",
-    })
-    .url(),
-  MEDIAWIKI_INTERNAL_ENDPOINT: z
-    .string({
-      required_error: "😱 You forgot to add a MediaWiki internal endpoint!",
     })
     .url(),
   MEDIAWIKI_ENDPOINT: z
@@ -39,18 +24,6 @@ const envSchema = z.object({
       required_error: "😱 You forgot to add a MediaWiki endpoint!",
     })
     .url(),
-  MW_ADMIN_USERNAME: z
-    .string({
-      required_error: "😱 You forgot to add a MediaWiki admin username!",
-    })
-    .trim()
-    .min(1),
-  MW_ADMIN_PASSWORD: z
-    .string({
-      required_error: "😱 You forgot to add a MediaWiki admin password!",
-    })
-    .trim()
-    .min(1),
   MW_BOT_USERNAME: z
     .string({
       required_error: "😱 You forgot to add a MediaWiki bot username!",
@@ -61,27 +34,14 @@ const envSchema = z.object({
       required_error: "😱 You forgot to add a MediaWiki bot password!",
     })
     .min(1),
-  SENTRY_DSN: z.string().optional(),
-  SENTRY_ENV_BACKEND: z.string().optional(),
-  NODE_ENV: z.enum(["development", "production", "test"]).default(
-    "development",
-  ),
 });
 
 const envServer = envSchema.safeParse({
   WIKIADVISER_LANGUAGES: Deno.env.get("WIKIADVISER_LANGUAGES"),
-  WIKIADVISER_API_PORT: Deno.env.get("WIKIADVISER_API_PORT"),
-  WIKIADVISER_FRONTEND_ENDPOINT: Deno.env.get("WIKIADVISER_FRONTEND_ENDPOINT"),
   WIKIPEDIA_PROXY: Deno.env.get("WIKIPEDIA_PROXY"),
-  MEDIAWIKI_INTERNAL_ENDPOINT: Deno.env.get("MEDIAWIKI_INTERNAL_ENDPOINT"),
   MEDIAWIKI_ENDPOINT: Deno.env.get("MEDIAWIKI_ENDPOINT"),
-  MW_ADMIN_USERNAME: Deno.env.get("MW_ADMIN_USERNAME"),
-  MW_ADMIN_PASSWORD: Deno.env.get("MW_ADMIN_PASSWORD"),
   MW_BOT_USERNAME: Deno.env.get("MW_BOT_USERNAME"),
   MW_BOT_PASSWORD: Deno.env.get("MW_BOT_PASSWORD"),
-  SENTRY_DSN: Deno.env.get("SENTRY_DSN"),
-  SENTRY_ENV_BACKEND: Deno.env.get("SENTRY_ENV_BACKEND"),
-  NODE_ENV: Deno.env.get("NODE_ENV"),
 });
 
 if (!envServer.success) {

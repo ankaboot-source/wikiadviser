ade0ceb17620964796ddd8d14ee1ad97672ffcf9
Jaafoura <73950268+J43fura@users.noreply.github.com>
2025-05-08 13:22:27 +0100

    fix `restrict-mediawiki-access` function name (#848)
    * deploy "article"
    
    * rename createArticle.controller.ts to createArticle.ts
    
    * fix type and log errors
    
    * Update supabaseUserAuthorization.ts
    
    fix imports
    
    * fix `restrict-mediawiki-access` function name

--- supabase/functions/restrict-mediawiki-access/index.ts
+++ supabase/functions/restrict-mediawiki-access/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import authorizationMiddleware from "../_shared/middleware/auth.ts";
 import restrictMediawikiAccess from "./controller.ts";
 
-const functionName = "restrictMediawikiAccess";
+const functionName = "restrict-mediawiki-access";
 const app = new Hono().basePath(`/${functionName}`);
 
 app.get("/", authorizationMiddleware, restrictMediawikiAccess);

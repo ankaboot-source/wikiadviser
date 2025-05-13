cb34a10791f39c6b781a780f4cd55c35fb9494f8
Jaafoura <mehdi.jaafourag@gmail.com>
2025-05-13 19:45:37 +0100

    Create parsing-helper.test.ts

---
+++ supabase/functions/tests/parsing-helper.test.ts
@@ -0,0 +1,63 @@
+import { assertEquals } from "jsr:@std/assert@1";
+import {
+  addSourceExternalLinks,
+  addSourceTemplate,
+  convertSourceTemplateToLink,
+} from "../_shared/helpers/parsingRegex.ts";
+
+const basePath = new URL("./data/parsingWikitext/fixSources/", import.meta.url);
+
+const pageContentPath = new URL("pageContent.txt", basePath);
+const expectedAddSourceExternalLinksPath = new URL(
+  "expectedAddSourceExternalLinks.txt",
+  basePath
+);
+const expectedAddSourceTemplatePath = new URL(
+  "expectedAddSourceTemplate.txt",
+  basePath
+);
+const expectedConvertSourceTemplateToLinkPath = new URL(
+  "expectedConvertSourceTemplateToLink.txt",
+  basePath
+);
+
+const sourceLanguage = "fr";
+const pageContent = Deno.readTextFileSync(pageContentPath);
+const expectedAddSourceExternalLinks = Deno.readTextFileSync(
+  expectedAddSourceExternalLinksPath
+);
+Deno.test(
+  "Testing wikitext parsing (XML file): Adds source to external links",
+  () => {
+    assertEquals(
+      addSourceExternalLinks(pageContent, sourceLanguage),
+      expectedAddSourceExternalLinks
+    );
+  }
+);
+
+const expectedAddSourceTemplate = Deno.readTextFileSync(
+  expectedAddSourceTemplatePath
+);
+Deno.test(
+  "Testing wikitext parsing (XML file): Adds source to templates",
+  () => {
+    assertEquals(
+      addSourceTemplate(expectedAddSourceExternalLinks, sourceLanguage),
+      expectedAddSourceTemplate
+    );
+  }
+);
+
+const expectedConvertSourceTemplateToLink = Deno.readTextFileSync(
+  expectedConvertSourceTemplateToLinkPath
+);
+Deno.test(
+  "Testing wikitext parsing (XML file): Converts certain templates to links",
+  () => {
+    assertEquals(
+      convertSourceTemplateToLink(expectedAddSourceTemplate, sourceLanguage),
+      expectedConvertSourceTemplateToLink
+    );
+  }
+);

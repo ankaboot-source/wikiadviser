b745952048e120be7db0a1a54cf65caf381662cb
Jaafoura <mehdi.jaafourag@gmail.com>
2025-05-26 15:26:44 +0100

    Update 20250526085931_delete_user_and_anonymize_data_change_email.sql
    Update 'contact@wikiadviser.io' to 'deleted-user@wikiadviser.io' if it exists Else, insert 'deleted-user@wikiadviser.io'

--- supabase/migrations/20250526085931_delete_user_and_anonymize_data_change_email.sql
+++ supabase/migrations/20250526085931_delete_user_and_anonymize_data_change_email.sql
@@ -1,3 +1,30 @@
+DO $$
+BEGIN
+  -- Update 'contact@wikiadviser.io' to 'deleted-user@wikiadviser.io' if it exists--
+  UPDATE auth.users
+  SET
+    email = 'deleted-user@wikiadviser.io',
+    encrypted_password = crypt(gen_random_uuid()::text, gen_salt('bf')),
+    updated_at = now()
+  WHERE email = 'contact@wikiadviser.io';
+  
+  -- Else, insert 'deleted-user@wikiadviser.io' --
+  IF NOT FOUND THEN
+    INSERT INTO auth.users (
+      instance_id, id, aud, role, email, encrypted_password, created_at
+    )
+    VALUES (
+      '00000000-0000-0000-0000-000000000000',
+      gen_random_uuid(),
+      'authenticated',
+      'authenticated',
+      'deleted-user@wikiadviser.io',
+      crypt(gen_random_uuid()::text, gen_salt('bf')),
+      now()
+    );
+  END IF;
+END $$;
+
 CREATE OR REPLACE FUNCTION delete_user_and_anonymize_data() RETURNS void SECURITY definer
 SET search_path = public AS $$
 DECLARE anonymous_id UUID;

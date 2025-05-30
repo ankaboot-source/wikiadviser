d8c9ab022617ab8b17d6f43a98581d15dee28f0c
Jaafoura <73950268+J43fura@users.noreply.github.com>
2025-05-30 14:09:29 +0100

    upgrade supabase to v2.23.4 (#894)
    * fix seed.sql
    
    fix
    
    * upgrade supabase to v2.23.4
    
    * Update config.toml

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,16 +1,14 @@
--- Insert deleted user into auth.users
 INSERT INTO auth.users (
-  instance_id, id, aud, role, email, encrypted_password,created_at
+  instance_id, id, aud, role, email, encrypted_password, created_at
 )
-VALUES (
+SELECT
   '00000000-0000-0000-0000-000000000000',
   gen_random_uuid(),
   'authenticated',
   'authenticated',
   'deleted-user@wikiadviser.io',
   crypt(gen_random_uuid()::text, gen_salt('bf')),
-  now());
-
-UPDATE public.profiles
-SET avatar_url = '/favicon.ico'
-WHERE email IS 'deleted-user@wikiadviser.io';
+  now()
+WHERE NOT EXISTS (
+  SELECT 1 FROM auth.users WHERE email = 'deleted-user@wikiadviser.io'
+);

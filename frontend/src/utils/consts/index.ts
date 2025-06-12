b42bf4f92faa114b91581e1a41f646fd5b5f0420
Jaafoura <mehdi.jaafourag@gmail.com>
2025-06-12 11:30:28 +0100

    prettier

--- frontend/src/utils/consts/index.ts
+++ frontend/src/utils/consts/index.ts
@@ -1,4 +1,4 @@
-import ENV from "src/schema/env.schema";
+import ENV from 'src/schema/env.schema';
 
 export const SHARE_LINK_DAY_LIMIT = ENV.SHARE_LINK_DAY_LIMIT;
 
@@ -7,4 +7,4 @@ export const MAX_EMAIL_LENGTH = 20;
 export const HOURS_IN_DAY = 24;
 
 export const WIKIADVISER_PRICING_PAGE =
-  "https://www.wikiadviser.io/#oQgNgFoThyItKVpXykVyFzgOCQwFPVvfh";
+  'https://www.wikiadviser.io/#oQgNgFoThyItKVpXykVyFzgOCQwFPVvfh';

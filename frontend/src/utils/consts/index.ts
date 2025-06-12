74bfec518b1675625e24e9443dcb736080cc55d0
Jaafoura <mehdi.jaafourag@gmail.com>
2025-06-12 11:10:51 +0100

    comment env variables

--- frontend/src/utils/consts/index.ts
+++ frontend/src/utils/consts/index.ts
@@ -1,10 +1,10 @@
-import ENV from 'src/schema/env.schema';
+import ENV from "src/schema/env.schema";
 
-export const SHARE_LINK_DAY_LIMIT = ENV.SHARE_LINK_DAY_LIMIT || 2;
+export const SHARE_LINK_DAY_LIMIT = ENV.SHARE_LINK_DAY_LIMIT;
 
 export const MAX_EMAIL_LENGTH = 20;
 
 export const HOURS_IN_DAY = 24;
 
 export const WIKIADVISER_PRICING_PAGE =
-  'https://www.wikiadviser.io/#oQgNgFoThyItKVpXykVyFzgOCQwFPVvfh';
+  "https://www.wikiadviser.io/#oQgNgFoThyItKVpXykVyFzgOCQwFPVvfh";

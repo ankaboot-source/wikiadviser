8fa315d814f455071c88d85cb7e52acf442db95a
zied_dhf <zieddhouaifi@gmail.com>
2025-06-20 20:06:20 +0100

    Dockerized Mediawiki

---
+++ mediawiki-docker/docker-entrypoint.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+set -e
+
+################# Load .env file ###########
+if [ -f .env ]; then
+    source .env
+fi
+################## Vars ####################
+
+MW_ADMIN_USER="Admin"
+MW_ADMIN_PASSWORD="admin#2025"
+MW_BOT_USER="wikiadviser-bot"
+
+mw_init_dump_fr="https://rcsxuyoogygnyjbwbrbb.supabase.co/storage/v1/object/sign/mediawiki-init/init-dump-fr.sql?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJtZWRpYXdpa2ktaW5pdC9pbml0LWR1bXAtZnIuc3FsIiwiaWF0IjoxNzQwNzM0MjA2LCJleHAiOjQ4NjI3OTgyMDZ9.Q5bEpxsrWFP0KF-rVJXmt4zK3ypU-1qmpIAislLx9bs"
+mw_init_dump_en="https://rcsxuyoogygnyjbwbrbb.supabase.co/storage/v1/object/sign/mediawiki-init/init-dump-en.sql?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJtZWRpYXdpa2ktaW5pdC9pbml0LWR1bXAtZW4uc3FsIiwiaWF0IjoxNzQwNzM0MTgzLCJleHAiOjQ4NjI3OTgxODN9.2Fw1v-5jTrPSDSxpavIUS3E45jZL8UjNoGlMbLOwHOg"
+
+# Convert space-separated string to array
+LANGUAGES=($LANGUAGES)
+LANG_ARRAY=("${LANGUAGES[@]:-fr}") # Languages of your wiki instances.
+
+############################################
+
+# Function to wait for MariaDB to be ready
+wait_for_db() {
+  local host="$1"
+  local port="$2"
+  echo "Waiting for MariaDB at $host:$port..."
+  while ! nc -z "$host" "$port"; do
+    sleep 1
+  done
+  echo "mediawiki_db is up!"
+}
+
+# Wait for the database
+wait_for_db "mediawiki_db" 3306
+
+echo "Running database setup..."
+
+# Database #recommended MySQL/Mariadb
+echo "Installing MariaDB-server..."
+echo ""
+apt install -y mariadb-server
+echo ""
+echo "MariaDB-server installed!"
+echo ""
+
+# Download mediawiki init dumps
+for ln in "${LANG_ARRAY[@]}"; do
+    dump_file="mw_init_dump_${ln}"
+    wget "${!dump_file}" -O ./init-dump-${ln}.sql
+done
+
+
+# Setup DB
+for ln in "${LANG_ARRAY[@]}"; do
+
+    DB_NAME="DB_NAME_${ln}"
+    DB_USER="DB_USER_${ln}"
+    DB_PASS="DB_PASS_${ln}"
+
+    echo "Creating base for your '${ln}' Wiki..."
+    echo ""
+    mariadb -h ${MARIADB_HOST} -u root -p${MARIADB_ROOT_PASSWORD} -e "CREATE DATABASE ${!DB_NAME};"
+    mariadb -h ${MARIADB_HOST} -u root -p${MARIADB_ROOT_PASSWORD} -e "CREATE USER '${!DB_USER}'@'%' IDENTIFIED BY '${!DB_PASS}';"
+    mariadb -h ${MARIADB_HOST} -u root -p${MARIADB_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${!DB_NAME}.* TO '${!DB_USER}'@'%' WITH GRANT OPTION;"
+    echo "Importing Init Dumps..."
+    mariadb -h ${MARIADB_HOST} -u root -p${MARIADB_ROOT_PASSWORD} -D ${!DB_NAME} < ./init-dump-${ln}.sql
+
+    echo "'${ln}' wiki setup completed!"
+done
+
+# Creating MW BotPassword
+for ln in "${LANG_ARRAY[@]}"; do
+    echo "Creating BotPassword for $ln wiki"
+    botpassword_output=$(php /var/www/mediawiki/wiki/$ln/maintenance/createBotPassword.php $MW_ADMIN_USER --appid $MW_BOT_USER --grants "basic,highvolume,import,editpage,editprotected,editmycssjs,editmyoptions,editinterface,editsiteconfig,createeditmovepage,uploadfile,uploadeditmovefile,patrol,rollback,blockusers,viewdeleted,viewrestrictedlogs,delete,oversight,protect,viewmywatchlist,editmywatchlist,sendemail,createaccount,privateinfo,mergehistory")
+done
+BOT_USERNAME=$(echo "$botpassword_output" | grep -oP "username:'\K[^']+")
+BOT_PASSWORD=$(echo "$botpassword_output" | grep -oP "password:'\K[^']+")
+
+# Save all credentials into a local file
+echo "Mediawiki admin user: $MW_ADMIN_USER" >> /tmp/MW_CREDENTIALS.txt
+echo "Mediawiki admin password: $MW_ADMIN_PASSWORD" >> /tmp/MW_CREDENTIALS.txt
+echo "Mediawiki BotPassword user: $BOT_USERNAME" >> /tmp/MW_CREDENTIALS.txt
+echo "Mediawiki BotPassword password: $BOT_PASSWORD" >> /tmp/MW_CREDENTIALS.txt
+
+
+# Execute the main container command
+exec "$@"
---
+++ mediawiki-docker/mediawiki-setup.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+################# Load .env file ###########
+if [ -f .env ]; then
+    source .env
+fi
+################# VARS #####################
+ulimit -n 9999
+
+curl -s "https://en.wikipedia.org/wiki/Special:Version" > ./version_page.html
+MW_VERSION=${MW_VERSION:-$(grep -oP 'MediaWiki\s\d+\.\d+\.\d+(-wmf\.\d+)?' version_page.html | head -n1 | awk '{print $2}')}
+
+MW_PROJECT_DIR=${MW_PROJECT_DIR:-"mediawiki"}
+MW_PORT=${MW_PORT:-"8080"}
+
+CONF_DIR="../" # Wikiadviser root folder
+
+# Convert space-separated string to array
+LANGUAGES=($LANGUAGES)
+MARIADB_HOST=$MARIADB_HOST
+
+LANG_ARRAY=("${LANGUAGES[@]:-fr}") # Languages of your wiki instances.
+
+MW_SECRET_KEY=$(openssl rand -hex 32)
+MW_UPGRADE_KEY=$(openssl rand -hex 8)
+
+REQUIRED_RESOURCES=("./LocalSettings.php" "./wiki-site.conf" "./ports.conf" "../assets" "../MyVisualEditor")
+##############################################
+
+# This function is common in the first install and upgrade process.
+common_setup() {
+    echo "Install Additional extensions"
+    echo ""
+    #PageForms
+    for ln in "${LANG_ARRAY[@]}"; do
+        git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/PageForms.git /var/www/${MW_PROJECT_DIR}/wiki/$ln/extensions/PageForms
+    done
+    #ExternalData
+    for ln in "${LANG_ARRAY[@]}"; do
+        git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/ExternalData.git /var/www/${MW_PROJECT_DIR}/wiki/$ln/extensions/ExternalData
+    done
+    #RegularToolTips
+    for ln in "${LANG_ARRAY[@]}"; do
+        git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/RegularTooltips.git /var/www/${MW_PROJECT_DIR}/wiki/$ln/extensions/RegularTooltips
+    done
+    #HTMLTags
+    for ln in "${LANG_ARRAY[@]}"; do
+        git clone https://gerrit.wikimedia.org/r/mediawiki/extensions/HTMLTags.git /var/www/${MW_PROJECT_DIR}/wiki/$ln/extensions/HTMLTags
+    done
+
+    CURRENT_DIR="$(pwd)" # save current directory
+
+    for ln in "${LANG_ARRAY[@]}"; do
+        cd /var/www/${MW_PROJECT_DIR}/wiki/$ln || exit
+        echo "Mediawiki Submodule update (${ln} wiki)..."
+        git submodule update --init --recursive # update extensions,skins...
+        composer update --no-dev
+        cd extensions/TemplateStyles && composer update --no-dev && cd ../..
+    done
+
+    for ln in "${LANG_ARRAY[@]}"; do
+        cd /var/www/${MW_PROJECT_DIR}/wiki/$ln/ || exit
+        mv composer.local.json-sample composer.local.json
+        composer install --no-dev --no-interaction
+        cd extensions/Wikibase && composer install --no-dev && cd ../..
+        php maintenance/run.php ./maintenance/update.php
+        php maintenance/run.php ./extensions/Wikibase/lib/maintenance/populateSitesTable.php
+        php maintenance/run.php ./extensions/Wikibase/repo/maintenance/rebuildItemsPerSite.php
+        php maintenance/run.php ./maintenance/populateInterwiki.php
+    done
+
+    cd $CURRENT_DIR # return to original path
+
+}
+
+
+########################################### INSTALL ##############################################################
+# Check required resources
+for resource in "${REQUIRED_RESOURCES[@]}"; do
+    if [ ! -e "$resource" ]; then
+        echo "Error: '$resource' does not exist."
+        exit 1
+    fi
+done
+echo ""
+echo "All required resources exist. Continuing script..."
+echo ""
+sleep 2
+
+apt -y update && apt -y upgrade
+
+# Perl & Ploticus (required for EasyTimeline extension)
+apt install -y perl
+apt install -y ploticus
+apt install -y fonts-freefont-ttf
+apt install -y ffmpeg # required for TimedMediaHandler
+apt install -y php-curl # Required for EmbedVideo
+
+# Apache2 setup
+mv /etc/apache2/ports.conf /etc/apache2/ports.conf.old
+MW_PORT="$MW_PORT" envsubst < ./ports.conf | tee /etc/apache2/ports.conf > /dev/null
+MW_PORT="$MW_PORT" MW_PROJECT_DIR="$MW_PROJECT_DIR" envsubst < ./wiki-site.conf | tee /etc/apache2/sites-available/wiki-site.conf > /dev/null
+a2ensite wiki-site.conf
+
+# PHP
+apt install -y php libapache2-mod-php php-mbstring php-mysql php-xml php-cli php-intl
+
+# composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Mediawiki Setup
+echo "Creating /var/www/${MW_PROJECT_DIR}..."
+mkdir -p "/var/www/${MW_PROJECT_DIR}"
+chown $USER:$USER /var/www/${MW_PROJECT_DIR}
+
+for ln in "${LANG_ARRAY[@]}"; do
+    echo "Install [$ln] mediawiki version ${MW_VERSION}"
+    sleep 2
+    git clone https://gerrit.wikimedia.org/r/mediawiki/core.git --branch wmf/$MW_VERSION /var/www/${MW_PROJECT_DIR}/wiki/$ln
+done
+
+# Change permissions to images folder
+for ln in "${LANG_ARRAY[@]}"; do
+    mkdir /var/www/${MW_PROJECT_DIR}/wiki/"$ln"/images/timeline
+    chown -R www-data:www-data /var/www/${MW_PROJECT_DIR}/wiki/"$ln"/images
+done
+
+# Copy wikiadviser resources into mediawiki
+for ln in "${LANG_ARRAY[@]}"; do
+    cp -r ./assets/*  "/var/www/${MW_PROJECT_DIR}/wiki/"$ln"/resources/assets"
+    cp -r ./MyVisualEditor  "/var/www/${MW_PROJECT_DIR}/wiki/"$ln"/extensions"
+done
+
+for ln in "${LANG_ARRAY[@]}"; do
+    DB_NAME="DB_NAME_${ln}"
+    DB_USER="DB_USER_${ln}"
+    DB_PASS="DB_PASS_${ln}"
+
+    SERVER_ENDPOINT="http://localhost:${MW_PORT}" URL_PATH="/wiki/${ln}" MARIADB_HOST=${MARIADB_HOST} LANGUAGE="${ln}" DB_NAME="${!DB_NAME}" DB_USER="${!DB_USER}" DB_PASS="${!DB_PASS}" MW_SECRET_KEY="${MW_SECRET_KEY}" MW_UPGRADE_KEY="${MW_UPGRADE_KEY}"  envsubst '$SERVER_ENDPOINT $URL_PATH $MARIADB_HOST $LANGUAGE $DB_NAME $DB_USER $DB_PASS $MW_SECRET_KEY $MW_UPGRADE_KEY' < ./LocalSettings.php > /var/www/${MW_PROJECT_DIR}/wiki/${ln}/LocalSettings.php
+
+    echo "[$ln] DB Name: ${!DB_NAME}" >> /tmp/MW_CREDENTIALS.txt
+    echo "[$ln] DB User: ${!DB_USER}" >> /tmp/MW_CREDENTIALS.txt
+    echo "[$ln] DB Password: ${!DB_PASS}" >> /tmp/MW_CREDENTIALS.txt
+done
+
+# Call common_setup
+common_setup
+
+echo  "Remove unnecessary files"
+rm ./version_page.html

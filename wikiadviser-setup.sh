8faef7e4a5d040a3a9dc8e3c3620cb3e9446a4ed
Zied Dhouaifi <60506128+zieddhf@users.noreply.github.com>
2025-07-02 10:18:46 +0100

    Update MW bot password in .env automatically (#932)
    * Update MW bot password in .env automatically
    
    * Mediawiki container healthcheck

--- wikiadviser-setup.sh
+++ wikiadviser-setup.sh
@@ -2,6 +2,9 @@
 
 ENV_DIR=("./" "./frontend/" "./supabase/functions/")
 ENV_PATH=("./.env" "./frontend/.env")
+SUPABASE_ENV_PATH="./supabase/functions/.env"
+MW_CREDENTIALS_FILE="./mediawiki-docker/MW_CREDENTIALS.txt"
+CONTAINER_NAME="mediawiki"
 
 export LANG=C
 # Verify log file is passed as an argument
@@ -16,6 +19,24 @@ if [[ "$(basename "$PWD")" != "wikiadviser" ]]; then
   exit 1
 fi
 
+# Function to check container health
+wait_for_container_healthy() {
+    echo "⌛ Waiting for container '$CONTAINER_NAME' to become healthy..."
+    while true; do
+        # Get container health status
+        health_status=$(docker inspect --format='{{.State.Health.Status}}' "$CONTAINER_NAME" 2>/dev/null)
+        if [ "$health_status" = "healthy" ]; then
+            echo "✅ Container is healthy"
+            return 0
+        fi
+     done
+}
+
+# Wait for container to be healthy
+if ! wait_for_container_healthy; then
+    exit 1
+fi
+
 LOGFILE="$1"
 
 # Copy .env.example to .env
@@ -27,11 +48,18 @@ done
 SUPABASE_PROJECT_URL=$(awk '/API URL:/ { print $NF }' "$LOGFILE")
 SUPABASE_SECRET_PROJECT_TOKEN=$(awk '/service_role key:/ { print $NF }' "$LOGFILE")
 
+# Extract MW bot password
+MW_BOT_USERNAME=$(awk '/\[en\] Mediawiki BotPassword user:/ { print $NF }' "$MW_CREDENTIALS_FILE")
+MW_BOT_PASSWORD=$(awk '/\[en\] Mediawiki BotPassword password:/ { print $NF }' "$MW_CREDENTIALS_FILE")
+
 # Replace Supabase variables within .env files
 for env in "${ENV_PATH[@]}"; do
   sed -i "s|^SUPABASE_PROJECT_URL=.*|SUPABASE_PROJECT_URL=$SUPABASE_PROJECT_URL|" $env
   sed -i "s|^SUPABASE_SECRET_PROJECT_TOKEN=.*|SUPABASE_SECRET_PROJECT_TOKEN=$SUPABASE_SECRET_PROJECT_TOKEN|" $env
 done
 
-echo "✅ ./.env, ./frontend/.env, ./supabase/functions/.env successfully generated from $LOGFILE"
+# Update MW bot password variables
+sed -i "s|^MW_BOT_USERNAME=.*|MW_BOT_USERNAME=$MW_BOT_USERNAME|" $SUPABASE_ENV_PATH
+sed -i "s|^MW_BOT_PASSWORD=.*|MW_BOT_PASSWORD=$MW_BOT_PASSWORD|" $SUPABASE_ENV_PATH
 
+echo "✅ ./.env, ./frontend/.env, ./supabase/functions/.env successfully generated from $LOGFILE"

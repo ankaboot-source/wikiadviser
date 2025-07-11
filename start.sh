4146ade2086f85bbbc906872d251441bba1859b8
Zied Dhouaifi <60506128+zieddhf@users.noreply.github.com>
2025-07-11 13:32:59 +0100

    Update start.sh (#947)

--- start.sh
+++ start.sh
@@ -8,10 +8,26 @@ else
   npm i # To install Supabase dependencies
 fi
 
+echo "Stopping local Supabase..."
+echo ""
+npx supabase stop
+echo ""
+echo "Starting local Supabase..."
+echo ""
 npx supabase start > supabase.log
+echo "Updating .env files..."
+echo ""
 bash ./generate-env.sh --supabase-creds supabase.log
+echo ""
+echo "Starting Mediawiki & Wikiadviser..."
+echo ""
 pushd docker && docker compose up -d && popd
+echo ""
+echo "Updating .env files..."
+echo ""
 bash ./generate-env.sh --bot-creds
+echo ""
+echo "Starting Supabasse Edge Functions..."
 npx supabase functions serve
 
 

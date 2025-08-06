5d5135702ffe1748167c1d74ae5c5282462a4293
TejBenamor <tejeddin@ankaboot.io>
2025-08-06 15:36:50 +0100

    Refactor email transporter configuration to dynamically set secure option based on SMTP port

--- supabase/functions/notification/utils/email.ts
+++ supabase/functions/notification/utils/email.ts
@@ -1,9 +1,11 @@
 import nodemailer from 'nodemailer';
 
+const smtpPort = Number(Deno.env.get('SMTP_PORT') ?? 587);
+
 export const transporter = nodemailer.createTransport({
   host: Deno.env.get('SMTP_HOST'),
   port: Number(Deno.env.get('SMTP_PORT') ?? 587),
-  secure: false,
+  secure: smtpPort === 465,// true only if port is 465
   auth: {
     user: Deno.env.get('SMTP_USER'),
     pass: Deno.env.get('SMTP_PASS'),

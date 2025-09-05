721835d24de3d37ba4820d24b5c7897dbd773ccc
Jaafoura <mehdi.jaafourag@gmail.com>
2025-09-05 13:33:10 +0100

    typo

--- frontend/src/pages/auth/UserAccountPage.vue
+++ frontend/src/pages/auth/UserAccountPage.vue
@@ -276,13 +276,13 @@ const isUninitializedUser = computed(
   () => isAnon.value && !Boolean(changeEmail.value),
 );
 const hasPassword = computed(() => Boolean(userStore.user?.has_password));
-const isEmailProvider = computed(() => userStore.user?.is_email_provider);
+const hasEmailProvider = computed(() => userStore.user?.is_email_provider);
 
 const stepperRef = ref();
 const computedStep = computed(() => {
   if (isUninitializedUser.value) return 1;
   else if (isAnon.value) return 2;
-  else if (!hasPassword.value && isEmailProvider.value) return 3;
+  else if (!hasPassword.value && hasEmailProvider.value) return 3;
   else return 0;
 });
 const step = ref();

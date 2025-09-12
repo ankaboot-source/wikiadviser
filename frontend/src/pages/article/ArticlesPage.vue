6ed89000278bb4aa9c65aff90b7fa4b147a6ab13
TejBenamor <tejeddin@ankaboot.io>
2025-09-12 14:32:19 +0100

    prettier

--- frontend/src/pages/article/ArticlesPage.vue
+++ frontend/src/pages/article/ArticlesPage.vue
@@ -11,7 +11,6 @@
           <div
             class="q-gutter-sm"
             :class="{ row: $q.screen.gt.sm, column: $q.screen.lt.md }"
-
             :style="$q.screen.lt.md ? 'width: 100%' : ''"
           >
             <q-btn
@@ -101,7 +100,6 @@
           <div
             class="q-gutter-sm"
             :class="{ row: $q.screen.gt.sm, column: $q.screen.lt.md }"
-
             :style="$q.screen.lt.md ? 'width: 100%' : ''"
           >
             <q-btn

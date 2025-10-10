468131ec2695bd0cdde5bfd4a7ba424492c9a849
TejBenamor <167573393+TejBenamor@users.noreply.github.com>
2025-10-10 11:31:24 +0100

    set cookie handling (#1114)

--- frontend/src/api/supabase.ts
+++ frontend/src/api/supabase.ts
@@ -1,5 +1,5 @@
 import { Cookies } from 'quasar';
-import { createBrowserClient } from '@supabase/ssr';
+import { createBrowserClient, CookieOptions } from '@supabase/ssr';
 import ENV from 'src/schema/env.schema';
 import { Database } from 'src/types';
 
@@ -7,9 +7,27 @@ const supabaseClient = createBrowserClient<Database>(
   ENV.SUPABASE_PROJECT_URL,
   ENV.SUPABASE_SECRET_PROJECT_TOKEN,
   {
-    cookies: Cookies,
-    cookieOptions: {
-      secure: true,
+    cookies: {
+      get(name: string) {
+        return Cookies.get(name);
+      },
+      set(name: string, value: string, options: CookieOptions) {
+        Cookies.set(name, value, {
+          ...options,
+          secure: true,
+          sameSite: 'Lax',
+          path: '/',
+          expires: options.maxAge
+            ? new Date(Date.now() + options.maxAge * 1000)
+            : undefined,
+        });
+      },
+      remove(name: string, options: CookieOptions) {
+        Cookies.remove(name, {
+          ...options,
+          path: '/',
+        });
+      },
     },
   },
 );

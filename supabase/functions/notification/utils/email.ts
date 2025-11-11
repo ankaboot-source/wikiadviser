0e244622e5e63e7b42ab5230e6eba781d9f9a488
Jaafoura <mehdi.jaafourag@gmail.com>
2025-11-11 14:56:12 +0100

    Update email.ts

--- supabase/functions/notification/utils/email.ts
+++ supabase/functions/notification/utils/email.ts
@@ -1,13 +1,16 @@
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
-const smtpPort = Number(Deno.env.get('SMTP_PORT') ?? 587);
+const host = Deno.env.get("SMTP_HOST");
+const port = Number(Deno.env.get("SMTP_PORT") ?? 587);
+const user = Deno.env.get("SMTP_USER");
+const pass = Deno.env.get("SMTP_PASS");
 
 export const transporter = nodemailer.createTransport({
-  host: Deno.env.get('SMTP_HOST'),
-  port: Number(Deno.env.get('SMTP_PORT') ?? 587),
-  secure: smtpPort === 465,// true only if port is 465
+  host,
+  port,
+  secure: port === 465, // true only if port is 465
   auth: {
-    user: Deno.env.get('SMTP_USER'),
-    pass: Deno.env.get('SMTP_PASS'),
+    user,
+    pass,
   },
 });

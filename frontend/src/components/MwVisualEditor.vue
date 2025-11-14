43aaeaf3304271898800d118265e85e3ea7bba2d
TejBenamor <tejeddin@ankaboot.io>
2025-11-14 10:22:17 +0100

    Add default AI model

--- frontend/src/components/MwVisualEditor.vue
+++ frontend/src/components/MwVisualEditor.vue
@@ -86,6 +86,10 @@ async function handleDiffChange(data: {
     diffHtml: data.diffHtml,
   };
 
+  if (miraStore.currentMiraBotId) {
+    body.miraBotId = miraStore.currentMiraBotId;
+  }
+
   await supabaseClient.functions.invoke(functionName, {
     method: 'PUT',
     body: JSON.stringify(body),

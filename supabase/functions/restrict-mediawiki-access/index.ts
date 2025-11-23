7507ec03370117a2525449222229484c70c1daf8
Jaafoura <mehdi.jaafourag@gmail.com>
2025-11-23 11:19:44 +0100

    restrict-mediawiki-access: remove auth middleware

--- supabase/functions/restrict-mediawiki-access/index.ts
+++ supabase/functions/restrict-mediawiki-access/index.ts
@@ -1,10 +1,9 @@
 import { Hono } from "hono";
-import authorizationMiddleware from "../_shared/middleware/auth.ts";
 import restrictMediawikiAccess from "./controller.ts";
 
 const functionName = "restrict-mediawiki-access";
 const app = new Hono().basePath(`/${functionName}`);
 
-app.get("/", authorizationMiddleware, restrictMediawikiAccess);
+app.get("/", restrictMediawikiAccess);
 
 Deno.serve(app.fetch);

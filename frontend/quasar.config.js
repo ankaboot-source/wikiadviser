b068ab209fc3befcbf17598e698fc5284a6335ec
TejBenamor <tejeddin@ankaboot.io>
2025-12-10 16:35:45 +0100

    Update configuration to use USE_MIRA environment variable for AI Assistant

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -84,7 +84,7 @@ module.exports = configure(function (/* ctx */) {
         SENTRY_ENV_FRONTEND: process.env.SENTRY_ENV_FRONTEND,
         POSTHOG_API_KEY: process.env.POSTHOG_API_KEY,
         POSTHOG_API_HOST: process.env.POSTHOG_API_HOST,
-        MIRA_AI_ASSISTANT: process.env.MIRA_AI_ASSISTANT,
+        USE_MIRA: process.env.USE_MIRA,
       },
       // rawDefine: {}
       // ignorePublicFolder: true,

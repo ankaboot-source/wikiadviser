791ae402c63037150e02d3df581ac193bb3d63df
Jaafoura <mehdi.jaafourag@gmail.com>
2026-03-05 21:39:14 +0100

    Update 20260220102155_secure_profiles.sql

--- supabase/migrations/20260220102155_secure_profiles.sql
+++ supabase/migrations/20260220102155_secure_profiles.sql
@@ -1,9 +1,7 @@
 drop policy "Public profiles are viewable by everyone."
 on "public"."profiles";
 
-alter policy "Enable users to view their own data only"
+create policy "Enable users to view their own data only"
 on "public"."profiles"
 to authenticated
-using (
-	(( SELECT auth.uid() AS uid) = id)
-);
+using ( auth.uid() = id );

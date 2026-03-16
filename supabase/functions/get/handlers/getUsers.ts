83d5722c40cd90fd4071e48c86262cbab11639b7
Jaafoura <mehdi.jaafourag@gmail.com>
2026-03-16 23:10:33 +0100

    Update getUsers.ts

--- supabase/functions/get/handlers/getUsers.ts
+++ supabase/functions/get/handlers/getUsers.ts
@@ -1,6 +1,7 @@
+import { Context } from "npm:hono@4.7.4";
 import createSupabaseAdmin from "../../_shared/supabaseAdmin.ts";
 
-export async function getUsers(c: any) {
+export async function getUsers(c: Context) {
   const supabaseAdmin = createSupabaseAdmin();
   const { articleId } = await c.req.json();
 

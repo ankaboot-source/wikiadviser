74ddd29b49b0b28c4fb26f2fe0208c4505b2bb14
Jaafoura <mehdi.jaafourag@gmail.com>
2026-08-21 11:12:02 +0100

    fix(ui): change 'Reviewing in batches...' to 'Reviewing...'

--- frontend/src/components/ReviewByMira.vue
+++ frontend/src/components/ReviewByMira.vue
@@ -19,7 +19,7 @@
         </template>
         <span v-if="!$q.screen.lt.md" class="review-label">
           <template v-if="miraStore.chainActive">
-            &nbsp;Reviewing in batches...
+            &nbsp;Reviewing...
           </template>
           <template v-else>
             &nbsp;Review by&nbsp;<span
